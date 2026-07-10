39d6a5c5f79e319188ac2dc8beeaef185daf874f
Alex Rosca <arosca@beyond.com>
2026-07-10 13:24:55 +0100

    Adding swagger

---
+++ src/main/java/com/logistics/common/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.logistics.common;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Delivery Tracker API")
+                        .version("v1")
+                        .description("Driver import, delivery event recording, and stats"));
+    }
+}
