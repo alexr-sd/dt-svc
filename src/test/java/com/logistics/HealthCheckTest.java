a925a48485e92bfad7f367d9fa2d208e74bd120f
Alex Rosca <arosca@beyond.com>
2026-07-13 10:17:34 +0100

    Updating Health check test

--- src/test/java/com/logistics/HealthCheckTest.java
+++ src/test/java/com/logistics/HealthCheckTest.java
@@ -2,12 +2,14 @@ package com.logistics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@AutoConfigureTestRestTemplate
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HealthCheckTest {
 
